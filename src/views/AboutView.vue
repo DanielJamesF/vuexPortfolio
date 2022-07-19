cf570b839f1e01818b1e97bda6504e80b2e3a88a
DanielJamesF <fredericksd17@gmail.com>
2022-07-19 09:37:34 +0200

    about

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,74 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="container-fluid">
+    <div class="about">
+      <h1 id="aboutHeadig">About</h1>
+    </div>
+    <div class="container">
+      <div class="row">
+        <div class="col-4">
+          <img id="image" src="../assets/aboutImage.png" alt="" />
+        </div>
+        <div class="col-8">
+          <div>
+            <h2>bio...</h2>
+          </div>
+          <div>
+            <p>
+              I am a student at life Choices Academy where I am studying coding
+              in order to become a Web Developer in the future. My passsion for
+              coding stems from my love of technology and anything related to
+              it. I am a hard worker and am able to work well in groups as I
+              believe that I am able to get along with people pretty well.
+            </p>
+            <br />
+            <h2>hobbies...</h2>
+            <p>
+              I enjoy playing soccer, whether it's eleven a side or five a side, listening to music, mostly rap however I do enjoy listing to other genres as well, reading
+              manga, watching anime, and going out with friends, I esecially enjoy spending time with those that I am close with.
+            </p>
+          </div>
+        <div class="container">
+          <h2>other...</h2>
+          <div class="row">
+            <div class="col">Date of Birth</div>
+            <div class="col">:</div>
+            <div class="col">02-01-07</div>
+          </div>
+          <div class="row">
+            <div class="col">Phone number</div>
+            <div class="col">:</div>
+            <div class="col">+27 658 057 987</div>
+          </div>
+          <div class="row">
+            <div class="col">Location</div>
+            <div class="col">:</div>
+            <div class="col">Cape Town</div>
+          </div>
+          <div class="row">
+            <h2>accounts...</h2>
+            <div class="col">
+              <a href="https://github.com/DanielJamesF">Github</a>
+            </div>
+            <div class="col">
+              <a href="https://www.linkedin.com/in/daniel-fredericks-85744023a/"
+                >Linkedin</a
+              >
+            </div>
+          </div>
+        </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
+
+<script>
+export default {};
+</script>
+
+<style>
+#image{
+  
+  max-width: 100%;
+}
+</style>
